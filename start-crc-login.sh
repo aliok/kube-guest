9579ae69abfe3eda1ccac894b4316a105735b789
Ali Ok <aliok@redhat.com>
2021-03-29 03:10:21 +0300

    Fix cluster name and context name for crc
    It is good to have separate names for local crc and remote crc. Since they're using the same URL, their cluster+context name goes same in ~/.kube/config. These changes fixes that.

--- start-crc-login.sh
+++ start-crc-login.sh
@@ -10,9 +10,24 @@ source ${DIR}/common.sh
 header_text "Printing cluster info"
 ssh lenovo "crc console --credentials"
 
-header_text "Logging in as kubeadmin"
+header_text "Deleting existing ${DIR}/crc.kubeconfig"
+rm ${DIR}/crc.kubeconfig || true
+
+header_text "Logging in as kubeadmin with a new kubeconfig file at ${DIR}/crc.kubeconfig"
 KUBEADMIN_PW=$(ssh lenovo "crc console --credentials --output json" | jq -r '.clusterConfig.adminCredentials.password')
-oc login -u kubeadmin -p ${KUBEADMIN_PW} https://api.crc.testing:6443
+KUBECONFIG=${DIR}/crc.kubeconfig oc login -u kubeadmin -p ${KUBEADMIN_PW} https://api.crc.testing:6443 --insecure-skip-tls-verify=true
+
+header_text "Renaming the cluster and the context in KUBECONFIG file"
+KUBECONFIG=${DIR}/crc.kubeconfig kubectl config rename-context default/api-crc-testing:6443/kube:admin crc-lenovo
+sed -i 's/name: api-crc-testing:6443/name: crc-lenovo/g' ${DIR}/crc.kubeconfig
+sed -i 's/cluster: api-crc-testing:6443/cluster: crc-lenovo/g' ${DIR}/crc.kubeconfig
+
+header_text "Merging KUBECONFIG file into the ~/.kube/config"
+KUBECONFIG=~/.kube/config:${DIR}/crc.kubeconfig kubectl config view --merge --flatten > ~/.kube/config
+
+header_text "Switching to new context"
+kubectx crc-lenovo
+
 
 error_text "Now open https://api.crc.testing:6443"
 header_text "kubeadmin password"
--- start-crc.sh
+++ start-crc.sh
@@ -11,18 +11,4 @@ source ${DIR}/common.sh
 header_text "Executing start on host"
 ssh lenovo -t '/home/aliok/host/start-crc.sh'
 
-header_text "Copying KUBECONFIG file into ${DIR}/crc.kubeconfig"
-scp lenovo:/home/aliok/.crc/machines/crc/kubeconfig ${DIR}/crc.kubeconfig
-
-header_text "Renaming the cluster and the context in new KUBECONFIG file"
-sed -i 's/cluster: crc/cluster: crc-lenovo/g' ${DIR}/crc.kubeconfig
-sed -i 's/name: crc/name: crc-lenovo/g' ${DIR}/crc.kubeconfig
-sed -i 's/name: admin/name: crc-lenovo/g' ${DIR}/crc.kubeconfig
-
-header_text "Merging KUBECONFIG file into the ~/.kube/config"
-KUBECONFIG=~/.kube/config:${DIR}/crc.kubeconfig kubectl config view --merge --flatten > ~/.kube/config
-
-header_text "Switching to new context"
-kubectx crc-lenovo
-
 error_text "Now run ${DIR}/start-crc-local-dns-config.sh"
