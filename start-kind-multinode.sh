424d19c7f9f14a5f3fbce66ce2475eea92ea8821
Ali Ok <aliok@redhat.com>
2025-07-17 17:22:18 +0300

    Update start multi node Kind script

--- start-kind-multinode.sh
+++ start-kind-multinode.sh
@@ -9,15 +9,18 @@ CLUSTER_NAME=lenovo
 source ${DIR}/common.sh
 
 header_text "Executing start on host"
-ssh $CLUSTER_HOST -t '/home/aliok/host/start-kind-multinode.sh'
+# following doesn't work because some env vars would be missing in the SSH session
+# see https://unix.stackexchange.com/questions/669157
+# ssh $CLUSTER_HOST -t '/home/aliok/host/start-kind-multinode.sh'
+ssh $CLUSTER_HOST -t "exec bash --login -c '/home/aliok/host/start-kind-multinode.sh'"
 
 header_text "Copying KUBECONFIG file into ${DIR}/kind.kubeconfig"
 scp $CLUSTER_HOST:/home/aliok/host/kind.kubeconfig ${DIR}/kind.kubeconfig
 
 header_text "Deleting old Kind stuff in KUBECONFIG file ~/.kube/config"
-KUBECONFIG=~/.kube/config kubectl config delete-user kind-${CLUSTER_HOST}
-KUBECONFIG=~/.kube/config kubectl config delete-context kind-${CLUSTER_HOST}
-KUBECONFIG=~/.kube/config kubectl config delete-cluster kind-${CLUSTER_HOST}
+KUBECONFIG=~/.kube/config kubectl config delete-user kind-${CLUSTER_HOST} || true
+KUBECONFIG=~/.kube/config kubectl config delete-context kind-${CLUSTER_HOST} || true
+KUBECONFIG=~/.kube/config kubectl config delete-cluster kind-${CLUSTER_HOST} || true
 
 # TODO: maybe change the cluster name and context name first?
 
